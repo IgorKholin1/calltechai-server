328c1398b0faa9b406ec91be13a705c569ce4ce0
IgorKholin1 <igorkholin89@gmail.com>
2025-04-11 16:56:06 -0700

    fix logger

--- logger.js
+++ logger.js
@@ -1,12 +1,18 @@
+// logger.js
 const winston = require('winston');
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) =>
-      ${timestamp} [${level.toUpperCase()}]: ${message}
-    )
+    winston.format.printf(function(info) {
+      const timestamp = info.timestamp;
+      const level = info.level;
+      const message = info.message;
+      return timestamp + ' [' + level.toUpperCase() + ']: ' + message;
+    })
   ),
   transports: [new winston.transports.Console()]
 });
+
 module.exports = logger;
