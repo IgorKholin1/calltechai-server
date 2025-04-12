b78bdb9bfc00075d4cb0740756f6ad3e5b5c6be3
IgorKholin1 <igorkholin89@gmail.com>
2025-04-12 12:48:08 -0700

    update voice responses

--- responses.js
+++ responses.js
@@ -1,15 +1,22 @@
-// responses.js
 const { VoiceResponse } = require('twilio').twiml;
+
+function wrapInSsml(text, languageCode) {
+  if (languageCode === 'ru-RU') {
+    return '<speak><prosody rate="medium" pitch="default">' + text + '</prosody></speak>';
+  }
+  return text;
+}
 
 function gatherNextThinking(res, finalAnswer, voiceName, languageCode) {
   const twiml = new VoiceResponse();
-  twiml.say({ voice: voiceName, language: languageCode },
-    languageCode === 'ru-RU'
-      ? "Спасибо! Подождите, я проверяю ваш запрос..."
-      : "Thanks! Let me check that for you..."
-  );
+
+  const thinkingMessage = languageCode === 'ru-RU'
+    ? wrapInSsml("Спасибо! Подождите, я проверяю ваш запрос...", languageCode)
+    : "Thanks! Let me check that for you...";
+
+  twiml.say({ voice: voiceName, language: languageCode }, thinkingMessage);
   twiml.pause({ length: 0.5 });
-  twiml.say({ voice: voiceName, language: languageCode }, finalAnswer);
+  twiml.say({ voice: voiceName, language: languageCode }, wrapInSsml(finalAnswer, languageCode));
   twiml.pause({ length: 0.5 });
   const gather = twiml.gather({
     input: 'speech',
@@ -19,18 +26,17 @@ function gatherNextThinking(res, finalAnswer, voiceName, languageCode) {
     method: 'POST',
     timeout: 10
   });
-  gather.say({ voice: voiceName, language: languageCode },
-    languageCode === 'ru-RU'
-      ? "Могу ли я еще чем-то помочь? Скажите 'поддержка' для оператора или задайте вопрос."
-      : "Anything else can I help you with? Say 'support' for a human, or just ask your question."
-  );
+  const followUp = languageCode === 'ru-RU'
+    ? wrapInSsml("Могу ли я еще чем-то помочь? Скажите 'поддержка' для оператора или задайте вопрос.", languageCode)
+    : "Anything else can I help you with? Say 'support' for a human, or just ask your question.";
+  gather.say({ voice: voiceName, language: languageCode }, followUp);
   res.type('text/xml');
   return res.send(twiml.toString());
 }
 
 function gatherShortResponse(res, message, voiceName, languageCode) {
   const twiml = new VoiceResponse();
-  twiml.say({ voice: voiceName, language: languageCode }, message);
+  twiml.say({ voice: voiceName, language: languageCode }, wrapInSsml(message, languageCode));
   twiml.pause({ length: 0.5 });
   const gather = twiml.gather({
     input: 'speech',
@@ -40,11 +46,10 @@ function gatherShortResponse(res, message, voiceName, languageCode) {
     method: 'POST',
     timeout: 10
   });
-  gather.say({ voice: voiceName, language: languageCode },
-    languageCode === 'ru-RU'
-      ? "Могу ли я еще чем-то помочь? Скажите 'поддержка' для оператора или задайте вопрос."
-      : "Anything else can I help you with? Say 'support' for a human, or just ask your question."
-  );
+  const followUp = languageCode === 'ru-RU'
+    ? wrapInSsml("Могу ли я еще чем-то помочь? Скажите 'поддержка' для оператора или задайте вопрос.", languageCode)
+    : "Anything else can I help you with? Say 'support' for a human, or just ask your question.";
+  gather.say({ voice: voiceName, language: languageCode }, followUp);
   res.type('text/xml');
   return res.send(twiml.toString());
 }
