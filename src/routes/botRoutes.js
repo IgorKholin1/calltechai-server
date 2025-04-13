db0cfe33fa21c5a627763cdf9aa4a3ea6a04ad5e
IgorKholin1 <igorkholin89@gmail.com>
2025-04-13 16:34:25 -0700

    fix botroutes

--- src/routes/botRoutes.js
+++ src/routes/botRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { handleBotMessage } = require('../src/controllers/botController');
+const { handleBotMessage } = require('../controllers/botController');
 
 const router = express.Router();
 
