e94616d893eab8150e0a3921166013ee2baeb3a9
IgorKholin1 <igorkholin89@gmail.com>
2025-06-07 21:22:05 -0700

    fix gpthand

--- src/controllers/handlers/gptHandler.js
+++ src/controllers/handlers/gptHandler.js
@@ -1,7 +1,7 @@
 const { OpenAI } = require('openai');
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const { callGpt } = require('../../utils/gpt');
-const { gptModels } = require('../../config');
+const { gptModels } = require('../../../config');
 
 async function callGptClarify(text, mode = 'friend', context = {}, contextLang = 'en') {
   let prompt = '';
