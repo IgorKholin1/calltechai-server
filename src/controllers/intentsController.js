85c04fb26d39dab580960c096b677bf2546580ff
IgorKholin1 <igorkholin89@gmail.com>
2025-07-07 23:52:56 -0700

    add log

--- src/controllers/intentsController.js
+++ src/controllers/intentsController.js
@@ -1,5 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_SERVICE_ROLE_KEY:', process.env.SUPABASE_SERVICE_ROLE_KEY);
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY 
